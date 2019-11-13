427108a6b2901c0ffb5fae7137215d712d277135
Artur Dovgar <evmajoar@gmail.com>
2019-11-13 22:12:52 +0200

    Готовая верстка + старт модалок

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,20 +1,74 @@
+var $slider = $( '.slider' );
+
 if ( typeof $.fn.slick === 'function' ) {
-  $( '.slider' ).each( function () {
+  $slider.each( function () {
 
     let $that = $( this );
 
+    // Отзывы
+    if ( $that.hasClass( 'presentation__list' ) ) {
+      $that.slick({
+        mobileFirst: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        arrows: true,
+        prevArrow: $( '.presentation__arrow--prev' ),
+        nextArrow: $( '.presentation__arrow--next' ),
+        dots: true,
+        appendDots: $( '.presentation__dots' ),
+        lazyLoad: 'ondemand',
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: 'unslick'
+          }
+        ]
+      });
+
+      // $that.find( '.slick-current' ).index();
+      // console.log($that.find( '.slick-current' ).index());
+    }
+
+    // Отзывы
+    if ( $that.hasClass( 'reviews__list' ) ) {
+      $that.slick({
+        mobileFirst: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        arrows: true,
+        prevArrow: $( '.reviews__arrow--prev' ),
+        nextArrow: $( '.reviews__arrow--next' ),
+        dots: true,
+        appendDots: $( '.reviews__dots' ),
+        lazyLoad: 'ondemand',
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3
+            }
+          }
+        ]
+      });
+    }
+
+    // Партнёры
     if ( $that.hasClass( 'partners__list' ) ) {
       $that.slick({
         mobileFirst: true,
         infinite: false,
         slidesToShow: 2,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         draggable: false,
         arrows: true,
-        prevArrow: $( '.slider__button--prev' ),
-        nextArrow: $( '.slider__button--next' ),
+        prevArrow: $( '.partners__arrow--prev' ),
+        nextArrow: $( '.partners__arrow--next' ),
         dots: true,
-        appendDots: $( '.slider__dots' ),
+        appendDots: $( '.partners__dots' ),
         lazyLoad: 'ondemand',
         responsive: [
           {
@@ -29,3 +83,5 @@ if ( typeof $.fn.slick === 'function' ) {
 
   } );
 }
+
+$slider.css('visibility', 'visible');
